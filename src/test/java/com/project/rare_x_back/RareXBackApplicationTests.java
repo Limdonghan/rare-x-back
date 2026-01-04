b687da9c6e2aa50cb54a91ed2835a92071188244
ImDongHan <dim95985@gmail.com>
2026-01-05 02:48:34 +0900

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/project/rare_x_back/RareXBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rare_x_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RareXBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
