f37430d3654acc53087df627fa55e8998ab0157a
gubonyul <gubonyul123@gmail.com>
2026-01-05 16:05:28 +0900

    feat: User 엔티티 생성
    - users 테이블 매핑
    - JPA Auditing 적용 (생성/수정 시간 자동 관리_RareXBackApplication)
    - 회원가입 기능 구현을 위한 기본 엔티티 구조

--- src/main/java/com/project/rare_x_back/RareXBackApplication.java
+++ src/main/java/com/project/rare_x_back/RareXBackApplication.java
@@ -2,8 +2,10 @@ package com.project.rare_x_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // 엔티티 @CreatedDate, @LastModifiedDate 어노테이션 활성화 스위치 역할(시간 자동 기록)
 public class RareXBackApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/project/rare_x_back/entity/User.java
@@ -0,0 +1,67 @@
+package com.project.rare_x_back.entity;
+
+import com.project.rare_x_back.enums.ProviderType;
+import com.project.rare_x_back.enums.Role;
+import com.project.rare_x_back.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name ="user_id")
+    private Long userId;
+
+    @Column(name = "email", nullable = false, unique = true, length = 200)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "provider_type", nullable = false, length = 10)
+    private ProviderType providerType;
+
+    @Column(name = "role", nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "status", nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @CreatedDate    // 생성 시간 자동 기록
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate   // 수정 시간 자동 업데이트
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
