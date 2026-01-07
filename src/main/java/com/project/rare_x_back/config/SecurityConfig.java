8fe2081e9054683292a514a47e8326ffe84aa506
gubonyul <gubonyul123@gmail.com>
2026-01-07 20:25:35 +0900

    refactor: security config 파일 수정
    - RequiredArgsConstructor 어노테이션 추가

--- src/main/java/com/project/rare_x_back/config/SecurityConfig.java
+++ src/main/java/com/project/rare_x_back/config/SecurityConfig.java
@@ -17,8 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
