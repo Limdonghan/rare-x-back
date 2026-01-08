52c3367b9f0016e05025b0fb57d3c30b38fc35c5
joeunlee <sj92031@gmail.com>
2026-01-08 09:20:44 +0900

    fix : 상품관리 API에서 @Valid 검증 추가
    Valid 검증 추가로 Dto 수정, Controller 수정

--- src/main/java/com/project/rare_x_back/dto/request/CategoryCreateRequestDto.java
+++ src/main/java/com/project/rare_x_back/dto/request/CategoryCreateRequestDto.java
@@ -1,10 +1,12 @@
 package com.project.rare_x_back.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CategoryCreateRequestDto {
+    @NotBlank(message = "카테고리명을 입력해 주세요.")
     private String categoryName;
 }
--- src/main/java/com/project/rare_x_back/dto/request/ProductCreateRequestDto.java
+++ src/main/java/com/project/rare_x_back/dto/request/ProductCreateRequestDto.java
@@ -1,14 +1,27 @@
 package com.project.rare_x_back.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ProductCreateRequestDto {
+    @NotNull(message = "브랜드는 필수 선택해야합니다.")
     private Long brandId;
+
+    @NotNull(message = "카테고리는 필수 선택해야합니다.")
     private Long categoryId;
+
+    @NotBlank(message = "상품명은 필수 입니다.")
+    @Size(max = 20, message = "상품명은 20자 이내여야 합니다.")
     private String productName;
+
+    @NotBlank(message = "상품 설명을 입력해주세요.")
+    @Size(max = 100, message = "상품 설명은 100자 이내로 작성해주세요.")
     private String productDescription;
-    private int retailPrice;
+
+    @NotNull(message = "상품의 발매가를 입력해주세요.")
+    @PositiveOrZero(message = "발매가는 0원 이상이어야 합니다.")
+    private Integer retailPrice;
 }
