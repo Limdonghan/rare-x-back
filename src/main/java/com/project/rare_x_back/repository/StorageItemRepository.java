875402adb021bd4da08a425a11ef4490c4b006c9
gubonyul <gubonyul123@gmail.com>
2026-01-14 16:03:58 +0900

    feat: 보관 중 상품 목록 조회 API 구현 (STORAGE-003)
    - StorageItemService 생성
    - StorageRequestController에 목록 조회 API 추가
    - Fetch Join으로 N+1 쿼리 방지

--- src/main/java/com/project/rare_x_back/repository/StorageItemRepository.java
+++ src/main/java/com/project/rare_x_back/repository/StorageItemRepository.java
@@ -2,11 +2,17 @@ package com.project.rare_x_back.repository;
 
 import com.project.rare_x_back.entity.StorageItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StorageItemRepository extends JpaRepository<StorageItem, Long> {
 
-    // 사용자별 보관 상품 목록 조회
-    List<StorageItem> findByUserUserId(Long userId);
+    // Fetch Join으로 Product, Brand 함께 조회
+    @Query("SELECT s FROM StorageItem s " +
+            "JOIN FETCH s.product p " +
+            "JOIN FETCH p.brand " +
+            "WHERE s.user.userId = :userId")
+    List<StorageItem> findByUserUserId(@Param("userId") Long userId);
 }
