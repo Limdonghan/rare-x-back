83dfd233ec48b85ac0ca44e9a81420d1b262f587
gubonyul <gubonyul123@gmail.com>
2026-01-14 16:56:46 +0900

    docs: StorageItemRepository 주석 추가
    - StorageItemRepository의 Fetch Join 성능 최적화 원리 주석 추가

--- src/main/java/com/project/rare_x_back/repository/StorageItemRepository.java
+++ src/main/java/com/project/rare_x_back/repository/StorageItemRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public interface StorageItemRepository extends JpaRepository<StorageItem, Long> {
 
-    // Fetch Join으로 Product, Brand 함께 조회
+    // Fetch Join을 사용하여 StorageItem을 가져올 때 Product와 Brand 정보를 한 번에 묶어서 조회
+    // 이렇게 하면 나중에 데이터를 꺼내 쓸 때 DB에 다시 물어볼 필요가 없어 성능이 좋아짐
     @Query("SELECT s FROM StorageItem s " +
             "JOIN FETCH s.product p " +
             "JOIN FETCH p.brand " +
