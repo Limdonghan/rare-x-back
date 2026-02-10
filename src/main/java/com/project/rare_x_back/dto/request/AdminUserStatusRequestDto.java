11c5139becfe592fadd5eeeb9f0394af5c15d491
gubonyul <gubonyul123@gmail.com>
2026-02-10 20:15:04 +0900

    feat: 관리자 회원관리 DTO 추가
    - AdminUserListResponseDto
    - AdminUserDetailResponseDto
    - AdminUserStatusRequestDto

---
+++ src/main/java/com/project/rare_x_back/dto/request/AdminUserStatusRequestDto.java
@@ -0,0 +1,12 @@
+package com.project.rare_x_back.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdminUserStatusRequestDto {
+    @NotBlank(message = "변경할 상태를 입력해주세요.")
+    private String status;
+}
