e871a02822d6385b9742a5501d02dc829f5ba548
leejoeun <sj92031@gmail.com>
2026-02-12 16:22:14 +0900

    fix : S3 Config 수정
    S3 Config 인증 방식 수정

--- src/main/java/com/project/rare_x_back/config/AwsS3Config.java
+++ src/main/java/com/project/rare_x_back/config/AwsS3Config.java
@@ -3,7 +3,8 @@ package com.project.rare_x_back.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,15 +12,22 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class AwsS3Config {
 
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
     public S3Client s3Client(){
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
 
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
                 .build();
     }
 
